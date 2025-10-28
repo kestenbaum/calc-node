0c9e8e62dc3baa2ba3b71f24bd75f8433e72e097
Oleksii <kespa71for@gmail.com>
2025-10-28 15:26:40 +0100

    refactor(utils): add handleError function to checked(utils)

--- src/utils/checked.ts
+++ src/utils/checked.ts
@@ -1,12 +1,19 @@
+import { handleErrors } from "./error";
+
 const checkedAnswer = async (
   ask: (q: string) => Promise<string>,
   question: string
 ): Promise<string> => {
   let answer = "";
   while (answer !== "yes" && answer !== "no") {
-    answer = (await ask(question)).trim().toLowerCase();
-    if (answer !== "yes" && answer !== "no") {
-      console.log("You must choose yes or no");
+    try {
+      answer = (await ask(question)).trim().toLowerCase();
+
+      if (answer !== "yes" && answer !== "no") {
+        console.log("You must choose yes or no");
+      }
+    } catch (err: unknown) {
+      handleErrors(err, "Error reading input:")
     }
   }
 
@@ -22,12 +29,15 @@ const checkedPerson = async (
   while (true) {
     const input = await ask(question);
     const parsed = parseInt(input);
-
-    if (!isNaN(parsed) && parsed >= 1) {
-      count = parsed;
-      break;
+    try {
+       if (!isNaN(parsed) && parsed >= 1) {
+        count = parsed;
+        break;
     } else {
       console.log("Please enter a valid number greater than or equal to 1.");
+    }
+    } catch (err: unknown) {
+      handleErrors(err, "Error reading input:" )
     }
   }
 
